0724682bab516bd13cb5d40313e07d1299d5b162
sudipto baral <sudiptobaral.me@gmail.com>
2021-02-05 19:29:35 +0600

    task 1 implemented

--- cmd/buildexecute.go
+++ cmd/buildexecute.go
@@ -18,7 +18,10 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"regexp"
+	"runtime"
+	"strings"
 
 	"github.com/otiai10/copy"
 	"github.com/sirupsen/logrus"
@@ -32,8 +35,9 @@ var buildexecuteCmd = &cobra.Command{
 	Long:  `Builds the porject from the specified directory. First copies the files from the specified prohject, then builds the project accordingly. Currently only the copy function is supported`,
 	Run: func(cmd *cobra.Command, args []string) {
 		sourchePath := getSourcePath(cmd)
-		destinationPath := getDestinationPath()
+		destinationPath := getDestinationPath(cmd, sourchePath)
 		copyDir(sourchePath, destinationPath)
+		compileAsBin(cmd, sourchePath)
 
 	},
 }
@@ -43,32 +47,57 @@ func init() {
 
 	// Flags
 	buildexecuteCmd.PersistentFlags().StringP("copydir", "c", "", "Copies the content of the specified dirtectory")
+	buildexecuteCmd.PersistentFlags().StringP("builddir", "b", "", "Specify the destination directory")
+	buildexecuteCmd.PersistentFlags().StringP("exe", "e", "", "compile the code of the directory as a binary")
+}
+
+func compileAsBin(cmd *cobra.Command, sourcePath string) {
+	sourcePath = formatPath(sourcePath)
+	binaryName, _ := cmd.Flags().GetString("exe")
+
+	if runtime.GOOS == "windows" {
+		binaryName += ".exe"
+	}
+	_, err := exec.Command("go", "build", "-o", binaryName, sourcePath).CombinedOutput()
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	fmt.Println(binaryName, "created in ", sourcePath)
 
 }
 
 func copyDir(sourcePath, destinationPath string) {
 
-	var destinationFolder string = ""
-	re := regexp.MustCompile(`[/\\]`)
-	sourceSplit := re.Split(sourcePath, -1)
-	destinationSplit := re.Split(destinationPath, -1)
+	// Format the paths into a common delimiter '/'
+	sourcePath = formatPath(sourcePath)
+	destinationPath = formatPath(destinationPath)
 
-	// Build the destination folder for Copy function
-	// Delimeter of path is different in differnet operating system
-	// Build the path using forward slash because copy function accepts '/'
-	for i := range destinationSplit {
-		destinationFolder += destinationSplit[i] + "/"
+	sourceSplit := strings.Split(sourcePath, "/")
+	// if a folder is specified,
+	// append the name of the folder to the destination path
+	// to  create a folder of same name
+	// so that otiai10/copy can copy everyting into that folder
+	// in the destination
+	// if content of the file specified we do not append
+	if sourceSplit[len(sourceSplit)-1] != "" {
+
+		destinationPath += "/" + sourceSplit[len(sourceSplit)-1]
 	}
-	destinationFolder += sourceSplit[len(sourceSplit)-1]
 
 	//fmt.Println("Source folder :", sourcePath)
-	//fmt.Println("Destination  folder :", destinationFolder)
+	//fmt.Println("Destination  folder :", destinationPath)
 
-	err := copy.Copy(sourcePath, destinationFolder)
+	//Do not perform copy if the source and the destination is the same
+	if isDestinationUnderSource(sourcePath, destinationPath) {
+		fmt.Println("Destination folder is under the source directory. Did not copy.")
+		return
+	}
+	err := copy.Copy(sourcePath, destinationPath)
 	if err != nil {
 		logrus.Error(err)
 	} else {
-		fmt.Println("Files copied successfully")
+		fmt.Println("Copied to ", destinationPath)
 	}
 }
 
@@ -76,18 +105,79 @@ func getSourcePath(cmd *cobra.Command) string {
 	sourcePath, _ := cmd.Flags().GetString("copydir")
 
 	if sourcePath == "" {
-		fmt.Println(`Please specify the source path. Ex: gobuildercli buildexecute --copydir "configs/foldertocopy"`)
+		fmt.Println("Please specify a valid source path. Ex: gobuildercli buildexecute --copydir configs/foldertocopy")
 		os.Exit(0)
 	}
+
 	return sourcePath
 }
 
-func getDestinationPath() string {
+func getDestinationPath(cmd *cobra.Command, sourcePath string) string {
 
-	destinationPath, err := os.Getwd()
+	// if destination path is sepecified return the specified path
+	destinationPath, _ := cmd.Flags().GetString("builddir")
+
+	// if destination path is not specified
+	// return the current directory as destination
+	if destinationPath == "" {
+
+		return getCurrentDir()
+	}
+
+	return destinationPath
+}
+
+// Checks if the destination directory is under the source directory or not
+
+func isDestinationUnderSource(sourcePath, destinationPath string) bool {
+
+	sourceSplit := strings.Split(sourcePath, "/")
+	destinationSplit := strings.Split(destinationPath, "/")
+
+	// if the length of destination split is lower than source split
+	// then surely destination folder is not under the source folder
+	if len(destinationSplit) < len(sourceSplit) {
+		return false
+	}
+
+	flag := true
+	limit := len(sourceSplit)
+
+	for i := 0; i < limit; i++ {
+		if sourceSplit[i] != destinationSplit[i] {
+			flag = false
+			break
+		}
+	}
+	return flag
+
+}
+
+// Returns the path in the linux format
+func getCurrentDir() string {
+
+	currentWorkingDirtectory, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
-	return destinationPath
+	return currentWorkingDirtectory
+
+}
+
+func formatPath(path string) string {
+	var formattedPath string = ""
+	// Delimiter used in path string
+	re := regexp.MustCompile(`[/\\]`)
+	token := re.Split(path, -1)
+
+	// Build the path in the linux format
+	for i := range token {
+		formattedPath += token[i]
+		if i < len(token)-1 {
+			formattedPath += "/"
+		}
+	}
+
+	return formattedPath
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sudiptob2/go-builder-cli
 go 1.15
 
 require (
+	github.com/TwinProduction/go-color v0.0.3
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/otiai10/copy v1.4.2
 	github.com/sirupsen/logrus v1.2.0
